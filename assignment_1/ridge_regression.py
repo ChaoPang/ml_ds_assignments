e4f32f451dfb65bd260f69df87a52c393900b9a4
Chao Pang <ChaoPang229@gmail.com>
2021-02-03 23:01:01 -0500

    standardized features using training data's mean and std

--- assignment_1/ridge_regression.py
+++ assignment_1/ridge_regression.py
@@ -11,6 +11,9 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+FEATURE_NAMES = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'year made',
+                 'bias term']
+
 INPUT_FOLDER = 'hw1-data'
 X_TRAIN = 'X_train.csv'
 Y_TRAIN = 'y_train.csv'
@@ -18,31 +21,10 @@ X_TEST = 'X_test.csv'
 Y_TEST = 'y_test.csv'
 
 
-def normalize_data(x, y):
-    """
-    Scale the features data using the mean and standard deviation for features except the last
-    feature because it's added for the bias term. Scale the labels using the mean.
-
-    :param x:
-    :param y:
-    :return:
-    """
-
-    # Scale the features using mean and standard deviation except for the last feature
-    x_scaler = StandardScaler()
-    scaled_x = np.column_stack([x_scaler.fit_transform(x[:, 0:6]), x[:, 6:7]])
-
-    # Scale the label using mean
-    y_scaler = StandardScaler(with_std=False)
-    scaled_y = y_scaler.fit_transform(y)
-
-    return scaled_x, scaled_y
-
-
 def compute_ridge_regression_solution(x, y, _lambda):
     """
     Compute the ridge regression solution
-    
+
     :param x: feature matrix
     :param y: labels
     :param _lambda: hyperparameter for regularization term in ridge regression
@@ -81,9 +63,7 @@ def plot_df_lambda_w_rr(x, y, lambdas):
     :return:
     """
 
-    # Get the number of features for creating labels
-    _, num_features = np.shape(x)
-    w_rr_df_lambda_labels = [f'w{str(i)}' for i in range(1, num_features + 1)] + ['df']
+    w_rr_df_lambda_labels = FEATURE_NAMES + ['df']
 
     # compute the degree of freedom for lambda and the corresponding ridge regression solution
     w_rr_df_lambda_list = []
@@ -100,12 +80,13 @@ def plot_df_lambda_w_rr(x, y, lambdas):
     # Computing the lower bound and upper bound for the x axis limit
     x_axis_lower_bound = int(np.floor(w_rr_df_lambda_pd_unpivoted['df'].min()))
     x_axis_upper_bound = int(np.ceil(w_rr_df_lambda_pd_unpivoted['df'].max()))
-    fig, ax = plt.subplots()
-    ax.set_xlim(x_axis_lower_bound, x_axis_upper_bound + 1)
+
+    fig, ax = plt.subplots(figsize=(12, 9))
+    ax.set_xlim(x_axis_lower_bound, x_axis_upper_bound + 1.2)
 
     # Plot the lines
-    fig, ax = plt.subplots(figsize=(12, 9))
-    sns.lineplot(ax=ax, data=w_rr_df_lambda_pd_unpivoted, x='df', y='value', hue='variable')
+    sns.lineplot(ax=ax, data=w_rr_df_lambda_pd_unpivoted, x='df', y='value', hue='variable',
+                 legend=False)
 
     # Set the label to the corresponding line
     for feature_name in w_rr_df_lambda_pd_unpivoted.variable.unique().tolist():
@@ -143,7 +124,6 @@ def plot_lambda_rmse(x_train, y_train, x_test, y_test, lambdas):
     :return:
     """
     # Get the number of rows in the test set
-    num_of_rows, _ = np.shape(y_test)
     lambda_rmse = []
     for lambda_value in lambdas:
         # Compute the ridge regression solution
@@ -163,16 +143,17 @@ def plot_lambda_rmse(x_train, y_train, x_test, y_test, lambdas):
     plt.clf()
 
 
-def create_polynomial_terms(x, polynomial_order):
+def create_polynomial_features(x, polynomial_order):
     """
-    Create ploynomial terms based on the given ploynomial order, there is no interaction between
+    Create polynomial terms based on the given polynomial order, there is no interaction between
     features
     :param x:
     :param polynomial_order:
     :return:
     """
-    polynomial_terms = np.hstack([x[:, 0:6] ** (i + 1) for i in range(polynomial_order)])
-    return np.hstack([polynomial_terms, x[:, 6:7]])
+    # Skip the first order features
+    polynomial_features = [x[:, 0:6] ** (i + 1) for i in range(0, polynomial_order)]
+    return np.hstack(polynomial_features + [x[:, 6:7]])
 
 
 def plot_polynomial_lambda_rmse(x_train, y_train, x_test, y_test, polynomial_orders, lambdas):
@@ -199,19 +180,20 @@ def plot_polynomial_lambda_rmse(x_train, y_train, x_test, y_test, polynomial_ord
         # Translate the number to an ordinal representation
         ordinal_order = p.ordinal(polynomial_order)
 
-        # Create polynomial features for the training and test sets
-        augmented_x_train = create_polynomial_terms(x_train, polynomial_order)
-        augmented_x_test = create_polynomial_terms(x_test, polynomial_order)
+        # Create polynomial features for the train and test sets
+        augmented_x_train = create_polynomial_features(x_train, polynomial_order)
+        augmented_x_test = create_polynomial_features(x_test, polynomial_order)
+
+        scaled_x_train = scale_with_training_set(augmented_x_train, augmented_x_train)
+        scaled_x_test = scale_with_training_set(augmented_x_train, augmented_x_test)
 
         # Iterate through all lambda values
         for lambda_value in lambdas:
             # Compute the ridge regression solution
-            w_rr = compute_ridge_regression_solution(augmented_x_train, y_train,
-                                                     lambda_value)
+            w_rr = compute_ridge_regression_solution(scaled_x_train, y_train, lambda_value)
             # Compute the rmse in the test set
-            polynomial_order_lambda_rmse.append((f'{ordinal_order} order', lambda_value,
-                                                 compute_rmse(w_rr, augmented_x_test,
-                                                              y_test)))
+            rmse = compute_rmse(w_rr, scaled_x_test, y_test)
+            polynomial_order_lambda_rmse.append((f'{ordinal_order} order', lambda_value, rmse))
 
     # Create a pandas dataframe for ploting
     polynomial_order_lambda_rmse_pd = pd.DataFrame(polynomial_order_lambda_rmse,
@@ -225,6 +207,7 @@ def plot_polynomial_lambda_rmse(x_train, y_train, x_test, y_test, polynomial_ord
         sub = polynomial_order_lambda_rmse_pd[
             polynomial_order_lambda_rmse_pd['order'] == ordinal_order]
         _, lambda_value, rmse = sub[sub['lambda'] == sub['lambda'].median()].iloc[0].tolist()
+        print(f'For {sub.iloc[sub.rmse.argmin()].tolist()}')
         plt.text(lambda_value, rmse, ordinal_order, horizontalalignment='left', size='large')
 
     # Save the plot
@@ -232,6 +215,23 @@ def plot_polynomial_lambda_rmse(x_train, y_train, x_test, y_test, polynomial_ord
 
     # Clear the figure
     plt.clf()
+
+
+def scale_with_training_set(x_train, x_for_scaling):
+    """
+    Scale the polynomial features using the training data mean and standard deviation. This
+    function assumes the last dimension of x_for_scaling is the bias term that doesn't require
+    scaling.
+
+    :param x_train:
+    :param x_for_scaling:
+    :return:
+    """
+    standard_scaler = StandardScaler()
+    standard_scaler.fit(x_train[:, :-1])
+    scaled_x = np.hstack(
+        [standard_scaler.transform(x_for_scaling[:, :-1]), x_for_scaling[:, -1:]])
+    return scaled_x
 
 
 def main():
@@ -247,7 +247,7 @@ def main():
     x_train = x_train_pd.to_numpy()
     y_train = y_train_pd.to_numpy()
 
-    scaled_x_train, scaled_y_train = normalize_data(x_train, y_train)
+    scaled_x_train = scale_with_training_set(x_train, x_train)
 
     # Loading the test data
     x_test_pd = pd.read_csv(path.join(INPUT_FOLDER, X_TEST), sep=',', header=None)
@@ -256,26 +256,26 @@ def main():
     x_test = x_test_pd.to_numpy()
     y_test = y_test_pd.to_numpy()
 
-    scaled_x_test, scaled_y_test = normalize_data(x_test, y_test)
+    scaled_x_test = scale_with_training_set(x_train, x_test)
 
     # Problem 3 Task (a) for plotting the w_rr as a function of df(lambda)
     plot_df_lambda_w_rr(x=scaled_x_train,
-                        y=scaled_y_train,
+                        y=y_train,
                         lambdas=range(0, 5001))
 
     # Problem 3 Task (c) for plotting the rmse as a function of lambda
     plot_lambda_rmse(x_train=scaled_x_train,
-                     y_train=scaled_y_train,
+                     y_train=y_train,
                      x_test=scaled_x_test,
-                     y_test=scaled_y_test,
+                     y_test=y_test,
                      lambdas=range(0, 51))
 
     # Problem 3 Task (d) for plotting the rmse as a function of lambda for w_rr calculated for
     # different polynomial orders
-    plot_polynomial_lambda_rmse(x_train=scaled_x_train,
-                                y_train=scaled_y_train,
-                                x_test=scaled_x_test,
-                                y_test=scaled_y_test,
+    plot_polynomial_lambda_rmse(x_train=x_train,
+                                y_train=y_train,
+                                x_test=x_test,
+                                y_test=y_test,
                                 polynomial_orders=range(1, 4),
                                 lambdas=range(0, 101))
 
