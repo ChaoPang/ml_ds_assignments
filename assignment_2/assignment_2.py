f16de15cf3569f4cd2d422776cfec35d117f60fb
Chao Pang <ChaoPang229@gmail.com>
2021-02-26 23:28:57 -0500

    finished coding

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 import pandas as pd
 from os import path
@@ -18,7 +20,14 @@ BAYES_CLASSIFIER_X = path.join(BAYES_CLASSIFIER_DATA, 'X.csv')
 BAYES_CLASSIFIER_Y = path.join(BAYES_CLASSIFIER_DATA, 'y.csv')
 README = path.join(BAYES_CLASSIFIER_DATA, 'README')
 
-gaussian_process_data = path.join(INPUT_DATA, 'Gaussian_process')
+GAUSSIAN_PROCESS_DATA = path.join(INPUT_DATA, 'Gaussian_process')
+GP_X_TRAIN = path.join(GAUSSIAN_PROCESS_DATA, 'X_train.csv')
+GP_Y_TRAIN = path.join(GAUSSIAN_PROCESS_DATA, 'y_train.csv')
+GP_X_TEST = path.join(GAUSSIAN_PROCESS_DATA, 'X_test.csv')
+GP_Y_TEST = path.join(GAUSSIAN_PROCESS_DATA, 'y_test.csv')
+
+FEATURE_NAMES = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'year made',
+                 'bias term']
 
 
 class NaiveBayesClassifier(NamedTuple):
@@ -89,9 +98,12 @@ def naive_bayes_k_fold_validation(x, y):
     conf_matrix = confusion_matrix(np.concatenate(y_true), np.concatenate(y_pred))
     accuracy = np.sum(np.diagonal(conf_matrix)) / np.sum(conf_matrix)
 
+    tn, fp, fn, tp = conf_matrix.ravel()
+    print(f'tn: {tn}, fp: {fp}, fn: {fn}, tp: {tp}')
     print(f'Bayes Classifier Confusion matrix:\n {conf_matrix}')
     print(f'Bayes Classifier Accuracy: {accuracy}')
 
+    # Create a stem plot for lambdas for both classes in the same figure
     lambda_0_avg = np.average(lambda_0, axis=0)
     lambda_1_avg = np.average(lambda_1, axis=0)
 
@@ -123,7 +135,7 @@ def compute_logistic_regression_objective_function(x, y, w):
     :param w:
     :return:
     """
-    return np.sum(- np.log(1 + np.exp(np.array(- x @ w * y, dtype=np.float128))))
+    return np.sum(np.log(sigmoid(x @ w * y)))
 
 
 def train_logistic_regression(x, y, learning_rate=0.01 / 4600, iteration=1000):
@@ -209,12 +221,13 @@ def plot_logistic_regression(x, y, model_name, is_newton=False):
         y_true_all.append(y_test)
         y_pred_all.append(y_pred)
 
+    # Calculate confusion matrix and accuracy
     conf_matrix = confusion_matrix(np.concatenate(y_true_all), np.concatenate(y_pred_all))
     accuracy = np.sum(np.diagonal(conf_matrix)) / np.sum(conf_matrix)
-
     print(f'{model_name} Confusion matrix:\n {conf_matrix}')
     print(f'{model_name} Accuracy: {accuracy}')
 
+    # Plotting the learning objective against iteration over 10 runs
     learning_objective_pd = pd.DataFrame(learning_objectives_all, columns=['run_number',
                                                                            'iteration',
                                                                            'learning_objective'])
@@ -230,7 +243,72 @@ def plot_logistic_regression(x, y, model_name, is_newton=False):
     # Clear the figure
     plt.clf()
 
-    # plot_learning_objectives(learning_objective_pd, figure_path)
+
+def rbf_kernel(v_1, v_2, b):
+    """
+    RBF kernel implementation
+    :param v_1:
+    :param v_2:
+    :param b:
+    :return:
+    """
+    return np.exp(-np.linalg.norm(v_1 - v_2) ** 2 / b)
+
+
+def gaussian_process_regression(x_train, y_train, x_test, b, sigma_square):
+    """
+    Gaussian process for computing the mean and covariance matrix for the test data given the
+    training data
+
+    :param x_train:
+    :param y_train:
+    :param x_test:
+    :param b:
+    :param sigma_square:
+    :return:
+    """
+    n, m = np.shape(x_train)
+    n_t, _ = np.shape(x_test)
+
+    K_1 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_train, x_train)], (n, n))
+    K_2 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_test, x_train)], (n_t, n))
+    K_3 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_test, x_test)], (n_t, n_t))
+
+    mean = K_2 @ np.linalg.inv(np.identity(n) * sigma_square + K_1) @ y_train
+    cov = sigma_square + K_3 - K_2 @ np.linalg.inv(np.identity(n) * sigma_square + K_1) @ K_2.T
+
+    return mean, cov
+
+
+def compute_rmse(y_true, y_pred):
+    num_of_rows, _ = np.shape(y_true)
+    return np.sqrt(np.sum((y_true - y_pred) ** 2) / num_of_rows)
+
+
+def explore_hyperparameters_cross_validation(x_test, x_train, y_test, y_train):
+    """
+    Train a gaussian process using different combinations of b and sigma squared, and calculate
+    RMSE in the test set
+
+    :param x_test:
+    :param x_train:
+    :param y_test:
+    :param y_train:
+    :return:
+    """
+    b_values = list(range(5, 17, 2))
+    sigma_2_values = [s / 10 for s in range(1, 11)]
+    metrics = []
+    for b, sigma_2 in product(b_values, sigma_2_values):
+        mean, cov = gaussian_process_regression(x_train, y_train, x_test, b, sigma_2)
+        rmse = compute_rmse(y_test, mean)
+        metrics.append((b, sigma_2, round(rmse, 2)))
+        print(f'b: {b} sigma: {sigma_2} rmse: {rmse}')
+    metrics_pd = pd.DataFrame([(t[0], t[1], round(t[2], 2)) for t in metrics],
+                              columns=['b', 'sigma_squared', 'rmse'])
+    metrics_pd.pivot(index='b', columns='sigma_squared', values='rmse') \
+        .to_csv(path.join(GAUSSIAN_PROCESS_DATA, 'gaussian_process_rmse.csv'), index=False)
+    print(metrics_pd.sort_values('rmse'))
 
 
 def main():
@@ -238,16 +316,54 @@ def main():
     x = pd.read_csv(BAYES_CLASSIFIER_X, header=None, sep=',').values
     y = pd.read_csv(BAYES_CLASSIFIER_Y, header=None, sep=',').values
 
+    # Problem 2(a) and 2(b)
     naive_bayes_k_fold_validation(x, y)
 
     # add one extra dimension to x
     x = np.hstack([x, np.ones([np.shape(x)[0], 1]).astype(int)])
     # overwite the label 0 to 1 for logistic regression
     y[y == 0] = -1
-    # Plotting learning objectives for logistic regression
+
+    # Problem 2(c) Plotting learning objectives for logistic regression
     plot_logistic_regression(x, y, model_name='logistic regression')
-    # Plotting learning objectives for logistic regression using Newton's method
+
+    # Problem 2(d) and 2(e) Plotting learning objectives for logistic regression using Newton's
+    # method
     plot_logistic_regression(x, y, model_name='newton method', is_newton=True)
+
+    # Loading the training data for Gaussian Process
+    x_train_gpr = pd.read_csv(GP_X_TRAIN, sep=',', header=None).to_numpy()
+    y_train_gpr = pd.read_csv(GP_Y_TRAIN, sep=',', header=None).to_numpy()
+
+    # Loading the test data for Gaussian Process
+    x_test_gpr = pd.read_csv(GP_X_TEST, sep=',', header=None).to_numpy()
+    y_test_gpr = pd.read_csv(GP_Y_TEST, sep=',', header=None).to_numpy()
+
+    # Problem 3(a): calculate the RMSEs in the test set using Gaussian Process with different
+    # combinations of b and sigma squared
+    explore_hyperparameters_cross_validation(x_test_gpr, x_train_gpr, y_test_gpr, y_train_gpr)
+
+    # Problem 3(c): explore the 4th dimension only and plot the predictive mean of each training
+    # data point
+    # Take the 4th dimension only
+    x_train_gpr_car_weight = x_train_gpr[:, 3:4]
+    mean, cov = gaussian_process_regression(x_train_gpr_car_weight, y_train_gpr,
+                                            x_train_gpr_car_weight,
+                                            5, 2)
+
+    fig, ax = plt.subplots(figsize=(12, 9))
+    # Set common labels
+    ax.set_xlabel('Car weight')
+    ax.set_ylabel('Miles per gallon')
+
+    sns.scatterplot(ax=ax, x=np.squeeze(x_train_gpr_car_weight), y=np.squeeze(y_train_gpr))
+    sns.lineplot(ax=ax, x=np.squeeze(x_train_gpr_car_weight), y=np.squeeze(mean))
+
+    # Save the plot
+    plt.savefig(path.join(GAUSSIAN_PROCESS_DATA, 'car_weight_rmse.png'))
+
+    # Clear the figure
+    plt.clf()
 
 
 if __name__ == '__main__':
--- assignment_2/gaussian_process.py
+++ assignment_2/gaussian_process.py
@@ -1,78 +1,62 @@
 # import system module
 from os import path
-import inflect
 
 # import data related modules
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from itertools import product, permutations
-from numpy.linalg import norm
 
 # import plot modules
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-FEATURE_NAMES = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'year made',
-                 'bias term']
-
-INPUT_FOLDER = 'hw2-data/Gaussian_process'
-X_TRAIN = 'X_train.csv'
-Y_TRAIN = 'y_train.csv'
-X_TEST = 'X_test.csv'
-Y_TEST = 'y_test.csv'
-
-# +
-# Loading the training data
-x_train_pd = pd.read_csv(path.join(INPUT_FOLDER, X_TRAIN), sep=',', header=None)
-y_train_pd = pd.read_csv(path.join(INPUT_FOLDER, Y_TRAIN), sep=',', header=None)
-
-x_train = x_train_pd.to_numpy()
-y_train = y_train_pd.to_numpy()
-
-# Loading the test data
-x_test_pd = pd.read_csv(path.join(INPUT_FOLDER, X_TEST), sep=',', header=None)
-y_test_pd = pd.read_csv(path.join(INPUT_FOLDER, Y_TEST), sep=',', header=None)
-
-x_test = x_test_pd.to_numpy()
-y_test = y_test_pd.to_numpy()
+from assignment_2.assignment_2 import INPUT_FOLDER, X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, \
+    gaussian_process_regression, explore_hyperparameters_cross_validation
 
 
-# -
+def main():
+    """
+    Main function
+    :return:
+    """
 
-def rbf_kernel(v_1, v_2, b):
-    return -np.exp(np.linalg.norm(v_1 - v_2) ** 2 / b)
+    # Loading the training data
+    x_train_gpr = pd.read_csv(path.join(INPUT_FOLDER, X_TRAIN), sep=',', header=None).to_numpy()
+    y_train_gpr = pd.read_csv(path.join(INPUT_FOLDER, Y_TRAIN), sep=',', header=None).to_numpy()
+
+    # Loading the test data
+    x_test_gpr = pd.read_csv(path.join(INPUT_FOLDER, X_TEST), sep=',', header=None).to_numpy()
+    y_test_gpr = pd.read_csv(path.join(INPUT_FOLDER, Y_TEST), sep=',', header=None).to_numpy()
+
+    # Problem 3(a): calculate the RMSEs in the test set using Gaussian Process with different
+    # combinations of b and sigma squared
+    explore_hyperparameters_cross_validation(x_test_gpr, x_train_gpr, y_test_gpr, y_train_gpr)
+
+    # Problem 3(c): explore the 4th dimension only and plot the predictive mean of each training
+    # data point
+    # Take the 4th dimension only
+    x_train_gpr_car_weight = x_train_gpr[:, 3:4]
+    mean, cov = gaussian_process_regression(x_train_gpr_car_weight, y_train_gpr,
+                                            x_train_gpr_car_weight,
+                                            5, 2)
+
+    # +
+    fig, ax = plt.subplots(figsize=(12, 9))
+    # Set common labels
+    ax.set_xlabel('Car weight')
+    ax.set_ylabel('Miles per gallon')
+
+    sns.scatterplot(ax=ax, x=np.squeeze(x_train_gpr_car_weight), y=np.squeeze(y_train_gpr))
+    sns.lineplot(ax=ax, x=np.squeeze(x_train_gpr_car_weight), y=np.squeeze(mean))
+
+    # Save the plot
+    plt.savefig(path.join(INPUT_FOLDER, 'car_weight_rmse.png'))
+
+    # Clear the figure
+    plt.clf()
 
 
-def gaussian_process_regression(x_train, y_train, x_test, b, sigma_2):
-    n, m = np.shape(x_train)
-    n_t, _ = np.shape(x_test)
-
-    K_1 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_train, x_train)], (n, n))
-    K_2 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_test, x_train)], (n_t, n))
-    K_3 = np.reshape([rbf_kernel(i, j, b) for i, j in product(x_test, x_test)], (n_t, n_t))
-
-    mean = K_2 @ np.linalg.inv(np.identity(n) * sigma_2 + K_1) @ y_train
-    cov = sigma_2 + K_3 - K_2 @ np.linalg.inv(np.identity(n) * sigma_2 + K_1) @ K_2.T
-
-    return mean, cov
-
-
-def compute_rmse(y_true, y_pred):
-    num_of_rows, _ = np.shape(y_true)
-    return np.sqrt(np.sum((y_true - y_pred) ** 2) / num_of_rows)
-
-
-b_values = list(range(5, 17, 2))
-sigma_2_values = [s / 10 for s in range(1, 11)]
-
-metrics = []
-for b, sigma_2 in product(b_values, sigma_2_values):
-    mean, cov = gaussian_process_regression(x_train, y_train, x_test, b, sigma_2)
-    rmse = compute_rmse(y_test, mean)
-    metrics.append((b, sigma_2, rmse))
-#     print(f'b: {b} sigma: {sigma_2} rmse: {rmse}')
-
-metrics
-
-sorted(metrics, key=lambda t: t[2])
+if __name__ == '__main__':
+    """
+    Author: Chao Pang 
+    """
+    main()
